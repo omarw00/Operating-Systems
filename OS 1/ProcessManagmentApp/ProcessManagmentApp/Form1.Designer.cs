96ab3dc2c253141098bbcba54bb77de691519865
omarw00 <34513929+omarw00@users.noreply.github.com>
2023-05-22 23:28:38 +0300

    Add files via upload

---
+++ OS 1/ProcessManagmentApp/ProcessManagmentApp/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace ProcessManagmentApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 517);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Running Processes";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(21, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(378, 479);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(52, 548);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(352, 114);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Select a Process and Press \"Kill\" to kill the process";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Kill";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(498, 704);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Processes Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OS 1/ProcessManagmentApp/ProcessManagmentApp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProcessManagmentApp
+{
+    public partial class Form1 : Form
+    {
+        String Details = "{0,-20}{1,-20}";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Process[] CurrProcesses = Process.GetProcesses();
+
+        private void GetCurrProccesses()
+        {
+            String ID;
+            String PName;
+            listBox1.Items.Clear();
+            foreach (Process p in CurrProcesses)
+            {
+
+                ID = "" + p.Id + "";
+                PName = p.ProcessName;
+
+                listBox1.Items.Add(String.Format(Details, ID, PName));
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String ID;
+            String PName;
+            foreach (Process p in CurrProcesses)
+            {
+                
+                ID = "" + p.Id + "";
+                PName = p.ProcessName;
+
+                listBox1.Items.Add(String.Format(Details,ID,PName));
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(String.Format(Details, "Process ID ", "Process Name"));
+            GetCurrProccesses();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex < 1)
+            {
+                MessageBox.Show("Please select a Process to kill before clicking the 'Kill' button!");
+            }
+            else
+            {
+                CurrProcesses[listBox1.SelectedIndex].Kill();
+                GetCurrProccesses();
+
+           
+            }
+        }
+    }
+}
